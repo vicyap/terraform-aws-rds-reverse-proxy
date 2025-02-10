0f76a95e86e6301a2145bfaa14ab64a2416f2457
Victor Yap <vicyap24@gmail.com>
2025-02-10 13:32:18 -0600

    update versions.tf

--- versions.tf
+++ versions.tf
@@ -10,13 +10,5 @@ terraform {
       source  = "hashicorp/dns"
       version = "~> 3.4"
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.4"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.1.0"
-    }
   }
 }
